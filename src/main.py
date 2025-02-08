300252f78f75e4d5250c1bfb91c941ae8508ebbf
Tabaktom <tabaktom360@gmail.com>
2025-02-08 11:53:05 +0000

    file logging

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+
+import datetime
 import numpy as np
 import pandas as pd
 
 
 def print_hi():
     df = pd.DataFrame(np.random.randint(0, 100, size=(10, 4)), columns=list('ABCD'))
+
+    with open('file.txt', 'w') as f:
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        f.write(now)
     # Use a breakpoint in the code line below to debug your script.
     print(f'{df}')  # Press ⌘F8 to toggle the breakpoint.
 
